2d137be15fe6e7e94d0255f0c67624f16bba7d82
Joan Garcia Tormo <joan.garciaitormo@cudoventures.com>
2024-02-26 13:53:24 +0000

    added fetch.ai agent example

---
+++ fetchAI/agent.py
@@ -0,0 +1,22 @@
+from uagents import Agent, Context
+from requests import get
+
+# fetches the public IP address of this VM
+ipAddress = get('https://api.ipify.org').content.decode('utf8')
+
+# creates the agent
+helloWorld = Agent(
+        name="hello-world",
+        port=1234,
+        # seed is secret phrase you should change
+        seed="an example recovery phrase",
+        endpoint=["http://" + ipAddress + "/:8001/submit"],
+        )
+
+# sends a confirmation message once it starts running
+@helloWorld.on_event("startup")
+async def say_hello(ctx: Context):
+    ctx.logger.info('Fetch.AI agent set up correctly.\n Hello from Intercloud!')
+
+if __name__ == "__main__":
+    helloWorld.run()
